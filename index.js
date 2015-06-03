cfa56ee2c84ce9e42366be050856336b640b2c06
Joo Aun Saw <jasaw@dius.com.au>
2015-06-03 11:56:03 +1000

    iso and shutter speed configurable from web interface

--- index.js
+++ index.js
@@ -5,9 +5,26 @@ var io = require('socket.io')(http);
 var fs = require('fs');
 var path = require('path');
 var spawn = require('child_process').spawn;
+var bodyParser = require('body-parser');
 var proc;
+var interval = 3000;
+var raspistill_args = {
+    "-tl"  : interval,
+    "-t"   : 0,
+    "-ISO" : 100,
+    "-ss"  : 800,
+    "-awb" : "off",
+    "-awbg": "1,1",
+    "-q"   : 100,
+    "-th"  : "none",
+    "-n"   : null,
+    "-w"   : 5184,
+    "-h"   : 1944,
+    "-3d"  : "sbs",
+    "-o"   : "/tmp/image_stream.jpg"
+  };
 
-app.use('/', express.static(path.join(__dirname, 'stream')));
+app.use(bodyParser.json());
 
 app.get('/static/:file', function(req, res) {
   var static_file;
@@ -47,6 +64,19 @@ app.get('/', function(req, res) {
   res.sendfile(__dirname + '/index.html');
 });
 
+app.post('/update', function(req, res){
+  var responseString = 'OK';
+  console.log('body: ' + JSON.stringify(req.body));
+  for (var k in req.body)
+    raspistill_args['-'+k] = req.body[k];
+  console.log(JSON.stringify(raspistill_args));
+  if (proc) {
+    proc.kill();
+    proc = spawn('raspistill', serializeRaspistillArgs());
+  }
+  res.send(responseString);
+});
+
 var sockets = {};
 io.on('connection', function(socket) {
   sockets[socket.id] = socket;
@@ -69,6 +99,16 @@ http.listen(3000, function() {
   console.log('listening on *:3000');
 });
 
+function serializeRaspistillArgs() {
+  var args = [];
+  for (var k in raspistill_args) {
+    args.push(k)
+    if (raspistill_args[k] !== null)
+      args.push(raspistill_args[k]);
+  }
+  return args;
+}
+
 function numClients() {
   return Object.keys(sockets).length;
 }
@@ -82,8 +122,6 @@ function stopStreaming() {
 }
 
 function startStreaming(io) {
-  var interval = 3000;
-
   // if already watching file
   if (app.get('watchingFile')) {
     io.sockets.emit('liveStream', 'stream/image_stream.jpg?_t=' + (Math.random() * 100000));
@@ -91,13 +129,7 @@ function startStreaming(io) {
   }
 
   // start new watch
-  //var args = ["-tl", interval, "-t", 0, "-sh", 100, "-q", 100, "-th", "none", "-n", "-w", 5184, "-h", 1944, "-3d", "sbs", "-o", "/tmp/image_stream.jpg"];
-  var args = ["-tl", interval, "-t", 0, "-q", 100, "-th", "none", "-n", "-w", 5184, "-h", 1944, "-3d", "sbs", "-o", "/tmp/image_stream.jpg"];
-  //var args = ["-tl", interval, "-t", 0, "-sh", 100, "-cs", 0, "-q", 100, "-th", "none", "-n", "-w", 2592, "-h", 1944, "-o", "/tmp/image_stream.jpg"];
-  //var args = ["-tl", interval, "-t", 0, "-sh", 100, "-cs", 1, "-q", 100, "-th", "none", "-n", "-w", 2592, "-h", 1944, "-o", "/tmp/image_stream.jpg"];
-  //var args = ["-tl", interval, "-t", 0, "-cs", 0, "-q", 100, "-th", "none", "-n", "-w", 2592, "-h", 1944, "-o", "/tmp/image_stream.jpg"];
-  //var args = ["-tl", interval, "-t", 0, "-cs", 1, "-q", 100, "-th", "none", "-n", "-w", 2592, "-h", 1944, "-o", "/tmp/image_stream.jpg"];
-  proc = spawn('raspistill', args);
+  proc = spawn('raspistill', serializeRaspistillArgs());
   console.log('Watching for changes...');
   app.set('watchingFile', true);
   fs.watchFile('/tmp/image_stream.jpg', {interval: interval}, function(current, previous) {
